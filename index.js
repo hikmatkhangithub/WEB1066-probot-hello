35a57a24daa8b108e024820863008c677a0bca35
hikmat-khan <48687886+hikmatkhangithub@users.noreply.github.com>
2021-04-11 18:18:20 +0200

    Edited index.js, package.json
    Corrected the test cases

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  * @param {import('probot').Application} app - Probot's Application class.
  */
 module.exports = app => {
-
   // Get an express router to expose new HTTP endpoints
   const router = app.route('/probot')
 
@@ -13,8 +12,6 @@ module.exports = app => {
   const Registry = client.Registry
   const register = new Registry()
   const collectDefaultMetrics = client.collectDefaultMetrics
-  
-  // Probe every 5th second.
 
   collectDefaultMetrics({register,
     timeout: 5000,
@@ -53,7 +50,7 @@ module.exports = app => {
   })
 
    // Ping router
-   router.get('/ping', (req, res) => {
+  router.get('/ping', (req, res) => {
     res.send('pong')
     app.log('pong response')
   })
@@ -79,7 +76,6 @@ module.exports = app => {
     }, 300000) // every 5 minutes (300000)
   }
 
-
   // Your code here
   app.log('Yay, the app was loaded!')
 
@@ -100,7 +96,7 @@ module.exports = app => {
       repository_full_name: context.payload.repository.full_name, // repository.full_name
       repository_name: context.payload.repository.name
     }
-    const duration = new Date(context.payload.check_run.completed_at) - new             Date(context.payload.check_run.started_at)
+    const duration = new Date(context.payload.check_run.completed_at) - new Date(context.payload.check_run.started_at)
 
     app.log('observation.action -> ' + observation.action)
     app.log('observation.name -> ' + observation.name)
